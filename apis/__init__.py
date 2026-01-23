e58f5810ceb49319e75cb21e4f3fd29a9fdbe87f
ShiroRikka <2985772684@qq.com>
2026-01-23 22:25:14 +0800

    ♻️ refactor(apis): 重构API模块导入和导出
    - 移除不再使用的BASE_URL和ALIVE_CHECK_ENDPOINT常量
    - 添加新的游戏服务器状态检查函数check_game_server_status
    - 添加二维码页面服务器状态检查函数check_qrcode_page_server_status
    - 更新__all__导出列表以匹配新的API函数

--- apis/__init__.py
+++ apis/__init__.py
@@ -4,6 +4,7 @@
 提供与舞萌DX相关API的集成。
 """
 
-from .diving_fish import BASE_URL, ALIVE_CHECK_ENDPOINT, check_server_status
+from .diving_fish import check_game_server_status
+from .wahlap_page_qrcode import check_qrcode_page_server_status
 
-__all__ = ["BASE_URL", "ALIVE_CHECK_ENDPOINT", "check_server_status"]
+__all__ = ["check_game_server_status", "check_qrcode_page_server_status"]
